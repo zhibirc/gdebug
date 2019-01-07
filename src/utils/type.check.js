1f7baf363d29db874c83723930f0e8781dbd5c1a
Yaroslav Surilov <y.surilov@infomir.com>
2019-01-07 15:31:36 +0200

    add basic type checks for utility purposes

--- src/utils/type.check.js
+++ src/utils/type.check.js
@@ -1,13 +1,73 @@
 // TODO: not reinventing the wheel and research the existed type-checkers.
 
-var toString = Object.prototype.toString;
+/**
+ * Get the object type string.
+ *
+ * @param {*} value
+ *
+ * @return {string}
+ */
+function getObjectType ( value ) {
+    return Object.prototype.toString.call(value)
+}
+
+
+/**
+ * Check to see if a value is an object.
+ *
+ * @param {*} value
+ *
+ * @return {boolean}
+ */
+function isObject(value) {
+    // null is object, hence the extra check
+    return value !== null && typeof value === 'object'
+}
+
+
+/**
+ * Check to see if a value is an object and only an object.
+ *
+ * @param {*} value
+ *
+ * @return {boolean}
+ */
+function isPlainObject ( value ) {
+    // eslint-disable-next-line
+    return isObject(value) && value.__proto__ === Object.prototype;
+}
+
+
+/**
+ * Check to see if a value is an array.
+ *
+ * @param {*} value
+ *
+ * @return {boolean}
+ */
+function isArray ( value ) {
+    return (
+        (typeof Array.isArray === 'function' && Array.isArray(value)) ||
+        getObjectType(value) === '[object Array]'
+    );
+}
+
+
+/**
+ * Check to see if a value is a primitive value.
+ *
+ * @param {*} value
+ *
+ * @return {boolean}
+ */
+function isPrimitive ( value ) {
+    return value !== function () { return this; }.call(value);
+}
+
 
 module.exports = {
-    isPrimitive: function ( value ) {
-        return value !== function () { return this; }.call(value);
-    },
-    isArray: Array.isArray,
-    isObject: function ( value ) {
-        return Object.prototype.toString.call(value).slice(8, -1).toLowerCase() === typeof null;
-    }
+    isPrimitive:   isPrimitive,
+    isArray:       isArray,
+    isObject:      isObject,
+    isPlainObject: isPlainObject
 };
