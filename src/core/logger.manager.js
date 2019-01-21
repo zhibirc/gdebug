a9e46d408609e40f750da92dadd87147f9c2d92c
Yaroslav Surilov <y.surilov@infomir.com>
2019-01-21 23:24:15 +0200

    extend manager's API with setLevel method

--- src/core/logger.manager.js
+++ src/core/logger.manager.js
@@ -1,5 +1,5 @@
 /**
- * @module log.manager
+ * @module logger.manager
  *
  * @license The MIT License (MIT)
  * @author Yaroslav Surilov <>
@@ -8,11 +8,34 @@
 'use strict';
 
 const
-    Logger = require('./logger'),
+    Logger     = require('./logger'),
+    types      = require('../configs/types'),
     logManager = {};
 
 
-logManager.getLogger = () => new Logger();
+/**
+ * Create new logger.
+ *
+ * @return {Logger} created logger
+ */
+logManager.getLogger = () => {
+    const logger = new Logger();
+
+    return logger;
+};
+
+
+/**
+ * Set logging level for all existed loggers.
+ *
+ * @param {number} level - logging level to set
+ *
+ * @example
+ * logManager.setLevel(logger.ERROR); // TODO: add uniform point for getting log levels
+ */
+logManager.setLevel = level => {
+
+};
 
 
 module.exports = logManager;
